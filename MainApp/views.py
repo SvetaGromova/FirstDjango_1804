cb1fdabeba0d58ef1ae2ef0a0ec10e3aab0d5b95
Svetlana <sveta2134@yandex.ru>
2023-04-21 11:38:11 +0300

    Finished Project

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,72 +1,53 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
 
-author = {
-       "name" : "Иван",
-       "middle": "Петрович",
-       "surname": "Иванов",
-       "phone": "8-923-600-01-02",
-       "email": "vasya@mail.ru"
-}
-
-
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 7, "name": "Картофель фри" ,"quantity":0},
-   {"id": 8, "name": "Кепка" ,"quantity":124},
-]
-
-
-# Create your views here.
 def home(request):
-
     context = {
         "name": 'Петров Николай Иванович',
         "email": "my_mail@mail.ru"
-
     }
     return render(request, "index.html", context)
 
-def about(request):
-    result = f"""
-       Имя: <b>{author["name"]}</b><br>
-       Отчество: <b>{author["middle"]}</b><br>
-       Фамилия: <b>{author["surname"]}</b><br>
-       телефон: <b>{author["phone"]}</b><br>
-       email: <b>{author["email"]}</b><br>
-       <a href='/'> Home </a>
-       """
 
+def about(request):
+    author = {
+        "name": "Иван",
+        "middle": "Петрович",
+        "surname": "Иванов",
+        "phone": "8-923-600-01-02",
+        "email": "vasya@mail.ru"
+    }
+    result = f"""
+    Имя: <b>{author["name"]}</b><br>
+    Отчество: <b>{author["middle"]}</b><br>
+    Фамилия: <b>{author["surname"]}</b><br>
+    телефон: <b>{author["phone"]}</b><br>
+    email: <b>{author["email"]}</b><br>
+    <a href='/'> Home </a>
+    """
     return HttpResponse(result)
 
 
 def get_item(request, id):
-    for item in items:
-        if item['id'] == id:
-#             result = f"""
-#             <h1>Имя: {item["name"]} </h1>
-#             <p>Количество: {item['quantity']} </p>
-#             <a href='/items'> Назад </a>
-# """
-#             return HttpResponse(result)
-          context = {
-              'item': item
-    }
+    try:
+        item = Item.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Item with id={id} not found')
+    else:
+
+        context = {
+            'item': item
+     }
     return render(request, "item-page.html", context)
+    return HttpResponseNotFound(f'Item with id={id} not found')
 
 
 def list_items(request):
-    # result = "<h2>список товаров</h2><ol>"
-    # for item in items:
-    #     result += f"<li><a href='/item/{item['id']}'>{item['name']}</a></li>"
-    # result += '</ol>'
-    # return HttpResponse(result)
+    items = Item.objects.all()
     context = {
         "items": items
-
     }
-    
-    return render(request, "item-list.html", context)
+    return render(request, "items-list.html", context)
